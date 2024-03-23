f8b420bfe68da7620d1fe850d05b68cc57e96b6e
Eduardo Amaral <eduama15@gmail.com>
2024-03-22 21:55:46 -0300

    Novo

---
+++ Trab2.R
@@ -0,0 +1,12 @@
+dados <- read.table("http://sobrevida.fiocruz.br/dados/ctinca.dat", header = TRUE)
+head(dados)
+
+# Pacotes
+require(survival)
+require(muhaz)
+require(pander)
+require(flexsurv)
+require(hnp)
+require(ggplot2)
+require(survminer)
+require(parmsurvfit)
